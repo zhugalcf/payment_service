69fc7b57ddcb76949c2eafb495d4eb64558c85ce
Basimov Rustam <sombra1nf@yandex.ru>
2023-09-11 18:47:22 +0500

    Подключил редис к проекту

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
      */
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
     /**
