72c14925d53b9351c08b649d02202be5c99a9404
aliceklim <yuliya.klimovich@icloud.com>
2023-09-13 20:57:23 +0200

    migration fixed

--- src/main/java/faang/school/paymentservice/model/FreeAccountNumber.java
+++ src/main/java/faang/school/paymentservice/model/FreeAccountNumber.java
@@ -5,7 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 import java.math.BigInteger;
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor
@@ -13,12 +16,36 @@ import java.math.BigInteger;
 @Builder
 @Entity
 @Table(name="free_account_numbers")
-@IdClass(FreeAccountNumberKey.class)
 public class FreeAccountNumber {
-    @Id
-    @Column(name="type", nullable = false)
+
+    @EmbeddedId
+    private FreeAccountNumberKey id;
+
+    @Column(name="type", nullable = false, insertable = false, updatable = false)
     private AccountType accountType;
-    @Id
-    @Column(name="account_number", nullable = false)
+
+    @Column(name = "account_number", nullable = false, insertable = false, updatable = false)
     private BigInteger accountNumber;
+
+    @Embeddable
+    @AllArgsConstructor
+    public static class FreeAccountNumberKey implements Serializable {
+        @Column(name = "type", nullable = false)
+        private AccountType accountType;
+
+        @Column(name = "account_number", nullable = false)
+        private BigInteger accountNumber;
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof FreeAccountNumberKey)) return false;
+            FreeAccountNumberKey that = (FreeAccountNumberKey) o;
+            return Objects.equals(accountType, that.accountType) &&
+                    Objects.equals(accountNumber, that.accountNumber);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(accountType, accountNumber);
+        }
+    }
 }
--- src/main/resources/db/changelog/changeset/payment_V002__account_num_generator.sql
+++ src/main/resources/db/changelog/changeset/payment_V002__account_num_generator.sql
@@ -1,11 +1,11 @@
-CREATE TABLE free_account_numbers {
+CREATE TABLE free_account_numbers (
    type                  VARCHAR(255) NOT NULL,
    account_number        NUMERIC NOT NULL,
    CONSTRAINT pk_free_account_numbers PRIMARY KEY (type, account_number)
-};
+);
 
-CREATE TABLE account_number_sequence {
+CREATE TABLE account_number_sequence (
    id                    SMALLSERIAL PRIMARY KEY,
    type                  VARCHAR(255) NOT NULL,
-   current_number        NUMERIC NOT NULL,
-};
+   current_number        NUMERIC NOT NULL
+);
