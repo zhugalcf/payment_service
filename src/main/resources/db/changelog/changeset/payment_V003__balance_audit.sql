754b00967e405f1e5ae3ab22994cd0bb7e227eaa
Rustam Nigmatullin <rustamnigma@mail.ru>
2023-09-28 15:34:53 +0300

    first edition

---
+++ src/main/resources/db/changelog/changeset/payment_V003__balance_audit.sql
@@ -0,0 +1,15 @@
+create table balance_audit(
+id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+sender_balance_id bigint not null references balance(id),
+getter_balance_id bigint not null references balance(id),
+lock_value varchar(244) not NULL,
+amount numeric not null,
+currency varchar(30),
+status integer not null check (status<3),
+created timestamptz DEFAULT current_timestamp,
+clear_scheduled_at timestamptz
+);
+
+CREATE UNIQUE INDEX balance_audit_unique_index
+    ON balance_audit (lock_value, sender_balance_id, getter_balance_id)
+    WHERE status = 0;
